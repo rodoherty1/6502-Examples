fa47ca201a14cf41e20bced9390414f724ac30b3
Rob O'Doherty <rodoherty1@gmail.com>
2020-04-25 20:32:06 +0100

    Interim commit.  Adding acme and cc65 folders.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 6502-Examples
+
+
+Very much a work in progress.
+
+Hoping to get some examples compiling with Acme and CL65.
+
+Originally forked from https://github.com/OldSkoolCoder/Tutorials
+
--- acme/one.asm
+++ acme/one.asm
@@ -2,8 +2,8 @@
 
 *=$0801
 
-    BYTE $0E, $08, $0A, $00, $9E, $20, $28
-    BYTE $32, $33, $30, $34, $29, $00, $00, $00
+    !byte $0E, $08, $0A, $00, $9E, $20, $28
+    !byte $32, $33, $30, $34, $29, $00, $00, $00
 
 *=$0900
 
@@ -11,18 +11,18 @@
 ;* Variables
 ;*************************************************
 
-SCREEN_Start_Location$ = $0400
+SCREEN_Start_Location = $0400
 
 VAR_BINARY = %01010101
 VAR_HEX = $1F
-VAR_OCTAL = @13
+VAR_OCTAL = 013
 VAR_DECIMAL = 123
 
 Start
     ldy #0
 @Looper
     tya
-    sta SCREEN_Start_Location$,y
+    sta SCREEN_Start_Location,y
     iny
     bne @Looper
     rts
