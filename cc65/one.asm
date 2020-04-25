a9670c75082475256db702f2d3af2f73bbcc72bf
Rob O'Doherty <rodoherty1@gmail.com>
2020-04-25 21:07:40 +0100

    cc65/one.asm is compiling

--- cc65/one.asm
+++ cc65/one.asm
@@ -1,28 +1,28 @@
 ; 10 SYS (2304)
 
-*=$0801
+; *=$0801
 
-    BYTE $0E, $08, $0A, $00, $9E, $20, $28
-    BYTE $32, $33, $30, $34, $29, $00, $00, $00
+;     BYTE $0E, $08, $0A, $00, $9E, $20, $28
+;     BYTE $32, $33, $30, $34, $29, $00, $00, $00
 
-*=$0900
+; *=$0900
 
 ;*************************************************
 ;* Variables
 ;*************************************************
 
-SCREEN_Start_Location$ = $0400
+SCREEN_Start_Location = $0400
 
 VAR_BINARY = %01010101
 VAR_HEX = $1F
-VAR_OCTAL = @13
+VAR_OCTAL = 013
 VAR_DECIMAL = 123
 
-Start
+Start:
     ldy #0
-@Looper
+@Looper:
     tya
-    sta SCREEN_Start_Location$,y
+    sta SCREEN_Start_Location,y
     iny
     bne @Looper
     rts
